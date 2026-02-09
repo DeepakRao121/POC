6be34e703e8a1db160a10b1e9489cb431c9205a6
devops <devops@lookfinity.com>
2026-02-09 15:48:38 +0530

    updated the deploy script

--- ec2InstanceRefresh/deploy.sh
+++ ec2InstanceRefresh/deploy.sh
@@ -1,51 +1,24 @@
 #!/usr/bin/env bash
 
+# Ask AWS profile
 read -p "Please enter the AWS Profile [default]? " AWS_PROFILE
 AWS_PROFILE=${AWS_PROFILE:-default}
 
-if [[ $AWS_PROFILE != "" ]]; then
-  PROFILE_CHECK=$([[ $(aws configure --profile $AWS_PROFILE list) && $? -eq 0 ]] && echo 1 || echo 0)
-else
-  PROFILE_CHECK=0
-fi
-
-if [[ $PROFILE_CHECK != 1 ]]; then
-  echo "Switching to \"default\""
-  AWS_PROFILE=default
-fi
-
 STACK_NAME=EC2-Instance-Rotation
+AWS_REGION=ap-south-1   # change if you want to ask this also
 
-PS3='Please enter the environment (enter corresponding number): '
-options=("dev" "qa" "qa2" "uat" "int" "int2" "production")
-select opt in "${options[@]}"; do
-  case $opt in
-  "dev" | "qa" | "uat" | "qa2" | "int2")
-    AWS_REGION=ap-south-1
-    BUCKET_NAME="cfn-templates-v2-ap-south-1-$opt"
-    ENV=$opt
-    break
-    ;;
-  "int")
-    AWS_REGION=us-east-1
-    BUCKET_NAME="cfn-templates-v2-$opt"
-    ENV=$opt
-    break
-    ;;    
-  "production")
-    AWS_REGION=ap-south-1
-    BUCKET_NAME=cfn-oneaboveall-templates-production
-    ENV=$opt
-    break
-    ;;
-  *) echo "invalid option" ;;
-  esac
-done
+# Ask bucket name
+read -p "Enter the S3 bucket name for artifacts: " BUCKET_NAME
 
-echo "Starting Deployment on $ENV"
+echo "Starting deployment..."
+echo "Profile : $AWS_PROFILE"
+echo "Region  : $AWS_REGION"
+echo "Bucket  : $BUCKET_NAME"
 
+# Build
 sam build --template-file master.yaml
 
+# Package
 sam package \
   --template-file .aws-sam/build/template.yaml \
   --output-template-file temp-template.template \
@@ -53,6 +26,7 @@ sam package \
   --region $AWS_REGION \
   --profile $AWS_PROFILE
 
+# Deploy
 sam deploy \
   --template-file temp-template.template \
   --disable-rollback \
@@ -60,9 +34,9 @@ sam deploy \
   --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM \
   --s3-bucket $BUCKET_NAME \
   --region $AWS_REGION \
-  --profile $AWS_PROFILE \
-  --parameter-overrides \
-  EnvironmentType="$ENV"
+  --profile $AWS_PROFILE
 
-# Clean Up
+# Cleanup
 rm -f temp-template.template
+
+echo "✅ Deployment completed"
